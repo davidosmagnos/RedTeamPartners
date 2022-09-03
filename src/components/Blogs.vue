584a5facc2c7cc098c46ede2519f0d7e01b0b94e
Markonometry <201820002@feualabang.edu.ph>
2022-09-03 18:16:51 +0800

    make news and blogs page dynamic

--- src/components/Blogs.vue
+++ src/components/Blogs.vue
@@ -4,17 +4,9 @@
         <h1 class="title">Cybersecurity <span class="redText">Blogs</span></h1>
         <span class="subtext">New release topics of Cybersecurity</span>
     </div>
-    <div class="blogs-wrapper">
-        <BlogComp title='Title:The title' description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <BlogComp title='Title:The title' description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <BlogComp title='Title:The title' description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <BlogComp title='Title:The title' description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <BlogComp title='Title:The title' description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <BlogComp title='Title:The title' description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <BlogComp title='Title:The title' description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <BlogComp title='Title:The title' description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <BlogComp title='Title:The title' description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <BlogComp title='Title:The title' description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
+    <div class="blogs-wrapper" id="blogs-wrapper">
+        <!-- <div><BlogComp title='Title:The title' description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/></div>
+        <div><BlogComp title='Title:The title' description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/></div> -->
     </div>
     <PartnerSlide/>
     <Footer/>
@@ -24,13 +16,29 @@
     import Footer from "./Footer.vue";
     import PartnerSlide from "./PartnerSlide.vue";
     import BlogComp from "./blogs-component.vue"
+    import {createApp} from 'vue'
     export default{
         name:"BlogsPage",
         components:{
             Header,
             Footer,
             PartnerSlide,
-            BlogComp,
+            // BlogComp,
+        },
+        mounted(){
+            for(let i=0;i<10;i++){
+                const newBlog = createApp(BlogComp,{
+                    link:'/',
+                    title:"Title:The title",
+                    description:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....',
+                })
+
+                const wrapper = document.createElement("div");
+                wrapper.classList.add("bloggs-cont");
+                newBlog.mount(wrapper);
+                document.querySelector("#blogs-wrapper").appendChild(wrapper)
+            }
+
         }
     }
 </script>
--- src/components/News.vue
+++ src/components/News.vue
@@ -4,17 +4,8 @@
         <h1 class="title">Cybersecurity <span class="redText">News</span></h1>
         <span class="subtext">Cyber Security Press Highlights</span>
     </div>
-    <div class="news-wrapper">
-        <NewsComp img="bg1.jpg" link='/' title="Title:The Title" description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <NewsComp img="bg2.png" link='/' title="Title:The Title" description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <NewsComp img="bg1.jpg" link='/' title="Title:The Title" description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <NewsComp img="bg1.jpg" link='/' title="Title:The Title" description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <NewsComp img="bg2.png" link='/' title="Title:The Title" description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <NewsComp img="bg3.jpg" link='/' title="Title:The Title" description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <NewsComp img="bg1.jpg" link='/' title="Title:The Title" description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <NewsComp img="bg2.png" link='/' title="Title:The Title" description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <NewsComp img="bg1.jpg" link='/' title="Title:The Title" description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
-        <NewsComp img="bg3.jpg" link='/' title="Title:The Title" description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/>
+    <div class="news-wrapper" id="news-wrapper">
+        <!-- <div><NewsComp img="bg1.jpg" link='/' title="Title:The Title" description='Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....'/></div> -->
     </div>
     <PartnerSlide/>
     <Footer/>
@@ -23,14 +14,30 @@
     import Header from "./Header.vue";
     import Footer from "./Footer.vue";
     import PartnerSlide from "./PartnerSlide.vue";
-    import NewsComp from "./news-component.vue"
+    import NewsComp from "./news-component.vue";
+    import {createApp} from 'vue';
     export default{
         name:"NewsPage",
         components:{
             Header,
             Footer,
             PartnerSlide,
-            NewsComp,
+            // NewsComp,
+        },
+        mounted(){
+            for(let i=0;i<10;i++){
+                const newNews = createApp(NewsComp,{
+                    img:"bg1.jpg", 
+                    link:'/' ,
+                    title:"Title:The Title",
+                    description:'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from....',
+                })
+
+                const wrapper = document.createElement("div");
+                wrapper.classList.add("newss-cont");
+                newNews.mount(wrapper);
+                document.querySelector("#news-wrapper").appendChild(wrapper);
+            }
         }
     }
 </script>
